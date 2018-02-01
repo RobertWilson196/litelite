65794e2c35a70c58d770d90c31c3ebf13f73750a
Robert Wilson <youmufan@gmail.com>
2018-01-31 20:58:02 -0500

    added more colors

--- app.js
+++ app.js
@@ -11,11 +11,25 @@ function toggleColor(e)
 {
     const element = e.target;
     const color = e.target.classList.color;
-    console.log(color);
-    element.classList.toggle('red'); 
+    element.classList.toggle('red');
+    if(color == 'red')
+    {
+        element.classList.toggle('blue'); 
+        console.log("changing from red to blue");
+    }
+    else if(color == 'blue')
+    {
+        element.classList.toggle('green');
+        console.log("changing from blue to green");
+    }
+    else if(color == 'green')
+    {
+        element.classList.remove('green');
+    }
 }
 
 function clearGrid(e)
 {
     console.log("Clearing the Grid.")
+    boxes.forEach((box,index)=>{box.classList.remove('red')})
 }
