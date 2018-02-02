7546c41a32b15d3af277cd0131a11dd99b30639a
Robert Wilson <youmufan@gmail.com>
2018-02-01 20:03:50 -0500

    added a dynamic grid using for loops

--- app.js
+++ app.js
@@ -17,16 +17,24 @@ function toggleColor(e)
     element.classList.toggle('red');
     if(color == 'red')
     {
+        element.classList.remove('red');
+        element.classList.remove('blue');
+        element.classList.remove('green');
         element.classList.toggle('blue'); 
         console.log("changing from red to blue");
     }
     else if(color == 'blue')
     {
+        element.classList.remove('red');
+        element.classList.remove('blue');
+        element.classList.remove('green');
         element.classList.toggle('green');
         console.log("changing from blue to green");
     }
     else if(color == 'green')
     {
+        element.classList.remove('red');
+        element.classList.remove('blue');
         element.classList.remove('green');
     }
 }
@@ -35,22 +43,44 @@ function clearGrid(e)
 {
     console.log("Clearing the Grid.")
     boxes.forEach((box,index)=>{box.classList.remove('red')})
+    boxes.forEach((box,index)=>{box.classList.remove('blue')})
+    boxes.forEach((box,index)=>{box.classList.remove('green')})
 }
 
 function changeGrid(e) 
 {
     e.preventDefault();
-    const gridSizeValue = +gridSizeFor.querySelector('input:checked').value;
+    const gridSizeValue = +gridSizeForm.querySelector('input:checked').value;
     const grid = makeGrid(gridSizeForm);
     main.innerHTML = ' '; // this clears the main area
-    main.appendChild(grid); //dynamically added a grid element
+    grid.forEach((row)=> {  //dynamically added a grid of rows
+        main.appendChild(row);
+    }); 
 
 }
 
-function makegrid(size) 
+function makeGrid(size) 
 {
-    const element = document.createElement('h1');
-    element.textContent = 'GRID';
-    return element;
-
+    const rows = [];
+    for(let i = 0; i < size; i += 1)
+    {
+        // make  a div with class = row
+        const row = document.createElement('div');
+        row.classList.add('row');
+        // make a div with class col-md-12 inside of row
+        const column = document.createElement('div');
+        column.classList.add('col-md-12');
+        row.appendChild(column);
+        // make 6 divs with class of box inside of col-md-12
+        for(let i = 0; i < size; i += 1) // const breaks the loop and var causes runtime bugs
+        {   
+            console.log(i);
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.addEventListener('click', toggleColor);
+            column.appendChild(box);
+        }    
+        rows.push(row);
+    }
+    return rows;
 }
